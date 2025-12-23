cc726f83824b5bacd952eb7f5b582da6701dc8f2
AkhilAajneeti <akhilaajneeti@gmail.com>
2025-12-23 14:12:03 +0530

    update some codesections

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,35 +1,60 @@
+// import api from "./api";
+
+// export const fetchAccounts = async () => {
+//   const res = await api.get("/api/accounts");
+//   return res.data;
+// };
+
+// export const createAccount = async (payload) => {
+//   const formData = new URLSearchParams();
+
+//   Object.entries(payload).forEach(([key, value]) => {
+//     if (value !== "" && value !== null && value !== undefined) {
+//       formData.append(key, value);
+//     }
+//   });
+
+//   const res = await api.post("/Account", formData, {
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//   });
+
+//   return res.data;
+// };
+
+// export const deleteAccount = async (id) => {
+//   const res = await api.delete(`/Account/${id}`, {
+//     deleted: true,
+//   });
+//   return res.data;
+// };
+// export const updateAccount = async (id, payload) => {
+//   const res = await api.put(`/Account/${id}`,payload);
+//   return res.data;
+// };
 import api from "./api";
 
+/* GET */
 export const fetchAccounts = async () => {
   const res = await api.get("/api/accounts");
   return res.data;
 };
 
+/* CREATE */
 export const createAccount = async (payload) => {
-  const formData = new URLSearchParams();
-
-  Object.entries(payload).forEach(([key, value]) => {
-    if (value !== "" && value !== null && value !== undefined) {
-      formData.append(key, value);
-    }
-  });
-
-  const res = await api.post("/Account", formData, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-
+  const res = await api.post("/api/accounts", payload);
   return res.data;
 };
 
-export const deleteAccount = async (id) => {
-  const res = await api.delete(`/Account/${id}`, {
-    deleted: true,
-  });
-  return res.data;
-};
+/* UPDATE */
 export const updateAccount = async (id, payload) => {
-  const res = await api.put(`/Account/${id}`,payload);
+  const res = await api.put(`/api/accounts?id=${id}`, payload);
+  return res.data;
+};
+
+/* DELETE */
+export const deleteAccount = async (id) => {
+  const res = await api.delete(`/api/accounts?id=${id}`);
   return res.data;
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,24 @@
+// import axios from "axios";
+
+// const username ="prashant_test_admin";
+// const password = "79pmYKy34C}";
+
+// const basicAuthToken = btoa(`${username}:${password}`);
+
+// const api = axios.create({
+//   baseURL:"",
+//   headers: {
+//     Authorization: `Basic ${basicAuthToken}`,
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
 import axios from "axios";
 
-const username ="prashant_test_admin";
-const password = "79pmYKy34C}";
-
-const basicAuthToken = btoa(`${username}:${password}`);
-
 const api = axios.create({
-  baseURL:"",
+  baseURL: "", // same domain (vercel)
   headers: {
-    Authorization: `Basic ${basicAuthToken}`,
     "Content-Type": "application/json",
   },
 });
